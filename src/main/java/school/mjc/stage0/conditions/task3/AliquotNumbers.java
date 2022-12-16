0a9257a8fa3f9c33e786300294273297d7b4f319
Sergey <spapoyan1987@gmail.com>
2022-12-16 14:31:09 +0400

    commit

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
+        if (first % 5 == 0 && second % 25 == 0) {
+            System.out.println("Aliquot");
+        } else System.out.println("Not aliquot");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+
+        if (!Character.isAlphabetic(character) || character == 'ł') {
+            System.out.println("wrong alphabet!");
+        } else if (Character.isAlphabetic(character) && (character == 'A' || character == 'a' ||
+                character == 'E' || character == 'e' ||
+                character == 'I' || character == 'i' ||
+                character == 'O' || character == 'o' ||
+                character == 'U' || character == 'u')) {
+            System.out.println("Vowel");
+        } else if (Character.isAlphabetic(character)) {
+            System.out.println("Consonant");
+        }
     }
 }
